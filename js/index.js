d1365d224c50571194c7332157c88a17de02c7f7
NSarvari <63661686+NSarvari@users.noreply.github.com>
2021-05-30 11:22:52 +0300

    add movie

--- js/index.js
+++ js/index.js
@@ -1,142 +1,67 @@
-// Movie Class: Represents a Movie
-class Movie {
-    constructor(title, genre, description, image,isbn) {
-      this.title = title;
-      this.genre = genre;
-      this.description = description;
-      this.image = image;
-      this.isbn = isbn;
-    }
+var selectedRow = null;
+function onFormSubmit(e){
+  Event.preventDefault();
+  var formData = readFormData();
+  if(selectedRow === null){
+    insertNewRecord(formData);
+  }else{
+    updateRecord(formData)
   }
-  
-  // UI Class: Handle UI Tasks
-  class UI {
-    static displayMovies() {
-      const movies = Store.getMovies();
-  
-      movies.forEach((movie) => UI.addMovieToList(movie));
-    }
-  
-    static addMovieToList(movie) {
-      const list = document.querySelector('#movie-list');
-  
-      const row = document.createElement('tr');
-  
-      row.innerHTML = `
-        <td>${movie.title}</td>
-        <td>${movie.genre}</td>
-        <td>${movie.description}</td>
-        <td>${movie.image}</td>
-        <td>${book.isbn}</td>
-        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
-      `;
-  
-      list.appendChild(row);
-    }
-  
-    static deleteMovie(el) {
-      if(el.classList.contains('delete')) {
-        el.parentElement.parentElement.remove();
-      }
-    }
-  
-    static showAlert(message, className) {
-      const div = document.createElement('div');
-      div.className = `alert alert-${className}`;
-      div.appendChild(document.createTextNode(message));
-      const container = document.querySelector('.container');
-      const form = document.querySelector('#book-form');
-      container.insertBefore(div, form);
-  
-      // Vanish in 3 seconds
-      setTimeout(() => document.querySelector('.alert').remove(), 3000);
-    }
-  
-    static clearFields() {
-      document.querySelector('#title').value = '';
-      document.querySelector('#genre').value = '';
-      document.querySelector('#description').value = '';
-      document.querySelector('#image').value = '';
-      document.querySelector('#isbn').value = '';
-    }
-  }
-  
-  // Store Class: Handles Storage
-  class Store {
-    static getMovies() {
-      let movies;
-      if(localStorage.getItem('movies') === null) {
-        movies = [];
-      } else {
-        movies = JSON.parse(localStorage.getItem('movies'));
-      }
-  
-      return books;
-    }
-  
-    static addMovie(movie) {
-      const movies = Store.getMovies();
-      movies.push(movie);
-      localStorage.setItem('movies', JSON.stringify(movies));
-    }
-  
-    static removeMovie(isbn) {
-      const movies = Store.getMovies();
-  
-      movies.forEach((movie, index) => {
-        if(movie.isbn === isbn) {
-          movies.splice(index, 1);
-        }
-      });
-  
-      localStorage.setItem('movies', JSON.stringify(movies));
-    }
-  }
-  
-  // Event: Display Movies
-  document.addEventListener('DOMContentLoaded', UI.displayMovies);
-  
-  // Event: Add a Book
-  document.querySelector('#movie-form').addEventListener('submit', (e) => {
-    // Prevent actual submit
-    e.preventDefault();
-  
-    // Get form values
-    const title = document.querySelector('#title').value;
-    const genre = document.querySelector('#genre').value;
-    const description = document.querySelector('#description').value;
-    const image = document.querySelector('#image').value;
-    const isbn = document.querySelector('#isbn').value;
-  
-    // Validate
-    if(title === '' || genre === '' || description === '' || image === '' || isbn === '') {
-      UI.showAlert('Please fill in all fields', 'danger');
-    } else {
-      // Instatiate movie
-      const movie = new Movie(title, genre, description,image, isbn);
-  
-      // Add Movie to UI
-      UI.addMovieToList(movie);
-  
-      // Add movie to store
-      Store.addMovie(movie);
-  
-      // Show success message
-      UI.showAlert('Movie Added', 'success');
-  
-      // Clear fields
-      UI.clearFields();
-    }
-  });
-  
-  // Event: Remove a Movie
-  document.querySelector('#movie-list').addEventListener('click', (e) => {
-    // Remove movie from UI
-    UI.deleteMovie(e.target);
-  
-    // Remove movie from store
-    Store.removeMovie(e.target.parentElement.previousElementSibling.textContent);
-  
-    // Show success message
-    UI.showAlert('Movie Removed', 'success');
-  });
+  resetForm();
+}
+
+//Read operation using this function
+function readFormData(){
+  var formData = {};
+  formData["title"] = document.getElementById("title").value;
+  formData["genre"] = document.getElementById("genre").value;
+  formData["description"] = document.getElementById("description").value;
+  formData["image"] = document.getElementById("image").value;
+  return formData;
+}
+
+//Create operation
+function insertNewRecord(data){
+  var table = document.getElementById("movies").getElementsByTagName('tbody')[0];
+  var newRow = table.insertRow(table.length);
+  var cell1 = newRow.insertCell(0);
+      cell1.innerHTML = data.title;
+  var cell2 = newRow.insertCell(1);
+      cell2.innerHTML = data.genre;
+  var cell3 = newRow.insertCell(2);
+      cell3.innerHTML = data.description;
+  var cell4 = newRow.insertCell(3);
+      cell4.innerHTML = data.image;
+  var cell5 = newRow.insertCell(4);
+      cell5.innerHTML = `<a href="#" onClick='onEdit(this)'>Edit</a>
+                      <a href="#" onClick='onDelete(this)'>Delete</a>`;
+}
+// To Reset the data of fill input
+function resetForm(){
+  document.getElementById('title').value = '';
+  document.getElementById('genre').value = '';
+  document.getElementById('description').value = '';
+  document.getElementById('image').value = '';
+  selectedRow = null;
+}
+// For Edit operation
+function onEdit(td){
+  selectedRow = td.parentElement.parentElement;
+  document.getElementById('title').value = selectedRow.cells[0].innerHTML;
+  document.getElementById('genre').value = selectedRow.cells[1].innerHTML;
+  document.getElementById('description').value = selectedRow.cells[2].innerHTML;
+  document.getElementById('image').value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData){
+  selectedRow.cells[0].innerHTML = formData.title;
+  selectedRow.cells[1].innerHTML = formData.genre;
+  selectedRow.cells[2].innerHTML = formData.description;
+  selectedRow.cells[3].innerHTML = formData.image;
+}
+function onDelete(td){
+  if(confirm('Are you sure you want to delete this record?')){
+      row = td.parentElement.parentElement;
+      document.getElementById('movies').deleteRow(row.rowIndex);
+      resetForm();
+  }    
+}
