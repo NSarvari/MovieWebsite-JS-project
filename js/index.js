f8051432d0aac2d3d0065f96db9f5668464d9f60
NSarvari <63661686+NSarvari@users.noreply.github.com>
2021-06-01 01:02:51 +0300

    CRUD issue solved

--- js/index.js
+++ js/index.js
@@ -1,89 +1,79 @@
-document.getElementById('form').addEventListener('submit', (e)=>{
-  e.preventDefault();
+var selectedRow = null
 
-  let title = document.getElementById("title").value;
-  let genre = document.getElementById("genre").value;
-  let description = document.getElementById("description").value;
-  let year = document.getElementById("year").value;
-  
-  createMovie(title,genre,description,year);
-});
-
-var movies= [];
-
-function createMovie(title,genre,description,year){
-  var movie={
-      title:title,
-      genre:genre,
-      description:description,
-      year:year
-  }
-  movies.push(movie);
-  readMovie(movie);
-  console.log(movies);
-  document.getElementById('form').reset();
+function onFormSubmit() {
+    if (validate()) {
+        var formData = readFormData();
+        if (selectedRow == null)
+            insertNewRecord(formData);
+        else
+            updateRecord(formData);
+        resetForm();
+    }
 }
 
-function readMovie(movie){
-  const list = document.querySelector('#movies');
-
-  const row = document.createElement('tr');
-
-  row.innerHTML = `
-    <td>${movie.title}</td>
-    <td>${movie.genre}</td>
-    <td>${movie.description}</td>
-    <td>${movie.year}</td>
-    <button class="edit" onClick="editMovie('${movie}')">Edit</button>
-    <button class="remove" onClick="deleteMovie('${movie}')">Delete</button>
-  `;
-
-  list.appendChild(row);
+function readFormData() {
+    var formData = {};
+    formData["title"] = document.getElementById("title").value;
+    formData["genre"] = document.getElementById("genre").value;
+    formData["description"] = document.getElementById("description").value;
+    formData["year"] = document.getElementById("year").value;
+    return formData;
 }
 
-function deleteMovie(i){
-  movies.splice(i,1);
-  readMovie(movie);
-  console.log(movies);
+function insertNewRecord(data) {
+    var table = document.getElementById("movieList").getElementsByTagName('tbody')[0];
+    var newRow = table.insertRow(table.length);
+    cell1 = newRow.insertCell(0);
+    cell1.innerHTML = data.title;
+    cell2 = newRow.insertCell(1);
+    cell2.innerHTML = data.genre;
+    cell3 = newRow.insertCell(2);
+    cell3.innerHTML = data.description;
+    cell4 = newRow.insertCell(3);
+    cell4.innerHTML = data.year;
+    cell4 = newRow.insertCell(4);
+    cell4.innerHTML = `<a href="#" onClick='onEdit(this)' style="margin-right:5px;color:green">Edit</a>
+                       <a onClick="onDelete(this)"><i class="fa fa-remove" style="margin-left:5px;color:red"></i></a>`;
 }
 
-function editMovie(index){
-  var moviehtml = document.getElementById('movies');
-  moviehtml.innerHTML= '';
-  for(var i=0; i<movies.length;i++){
-      if(i==index){
-          moviehtml.innerHTML+= `<div class="red">
-          TITLE: <input id="input2title"  placeholder="${movies[i].title}"><br><br>
-          GENRE: <input id="input2genre"  placeholder="${movies[i].genre}"><br><br>
-          DESCRIPTION: <input id="input2description"  placeholder="${movies[i].description}"><br><br>
-          YEAR: <input id="input2year"  placeholder="${movies[i].year}"><br><br>
-          <button class="edit" onClick="updateMovie('${i}')">Update</button>
-          <button  class="remove" onClick="readMovie(movie)">Cancel</button>
-          `
-      }else{
-          moviehtml.innerHTML+= `<div class="black"><p>TITLE: ${movies[i].title}</p>
-          <p>GENRE: ${movies[i].genre}</p>
-          <p>DESCRIPTION: ${movies[i].description}</p>
-          <p>YEAR: ${movies[i].image}</p>
-          <button disabled class="edit" onClick="editMovie('${i}')">Edit</button>
-          <button disabled class="remove" onClick="deleteMovie('${i}')">Delete</button> `
-      }
-  }
+function resetForm() {
+    document.getElementById("title").value = "";
+    document.getElementById("genre").value = "";
+    document.getElementById("description").value = "";
+    document.getElementById("year").value = "";
+    selectedRow = null;
 }
 
-function updateMovie(index){
-  var updateTitle= document.getElementById('input2title').value;
-  var updateGenre= document.getElementById('input2genre').value;
-  var updateDescription= document.getElementById('input2description').value;
-  var updateYear= document.getElementById('input2year').value;
+function onEdit(td) {
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("title").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("genre").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("description").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("year").value = selectedRow.cells[3].innerHTML;
+}
+function updateRecord(formData) {
+    selectedRow.cells[0].innerHTML = formData.title;
+    selectedRow.cells[1].innerHTML = formData.genre;
+    selectedRow.cells[2].innerHTML = formData.description;
+    selectedRow.cells[3].innerHTML = formData.year;
+}
 
-  if (updateTitle == '' || updateGenre=='' || updateDescription=='' || updateYear=='') {
-      alert("INCOMPLETE");
-  } else {
-      movies[index].title= updateTitle;
-      movies[index].genre= updateGenre;
-      movies[index].description= updateDescription;
-      movies[index].year= updateYear;
-      readMovie(movie);
-  }
+function onDelete(td) {
+    if (confirm('Are you sure to delete this record ?')) {
+        row = td.parentElement.parentElement;
+        document.getElementById("movieList").deleteRow(row.rowIndex);
+        resetForm();
+    }
+}
+function validate() {
+    isValid = true;
+    if (document.getElementById("title").value == "") {
+        isValid = false;
+        document.getElementById("titleValidationError").classList.remove("hide");
+    } else {
+        isValid = true;
+        if (!document.getElementById("titleValidationError").classList.contains("hide"))
+            document.getElementById("titleValidationError").classList.add("hide");
+    }
+    return isValid;
 }
